264a5ec086a4c04dd8551347eea1a100c332ee3b
danchikov_a <danchikovartem@gmail.com>
2022-11-01 02:05:54 +0300

    Added tests

---
+++ AssemblyBrowserTest/AssemblyBrowserTest.cs
@@ -0,0 +1,49 @@
+﻿using AssemblyBrowserApplication.Model;
+using AssemblyBrowserApplication.Service.impl;
+using NUnit.Framework;
+
+namespace AssemblyBrowserTest;
+
+public class AssemblyBrowserTests
+{
+    private readonly AssemblyServiceImpl _assemblyService = new("AssemblyBrowserApplication.dll");
+    private AssemblyModel _assemblyInfo = new ();
+    private const int MODEL_NAMESPACE = 4;
+    private const int ASSEMBLY_MODEL_CLASS = 0;
+    
+    [SetUp]
+    public void Setup()
+    {
+        _assemblyInfo = _assemblyService.GetAssemblyInfo();
+    }
+
+    [Test]
+    public void NamespaceAmountTest()
+    {
+        Assert.AreEqual(6, _assemblyInfo.Namespacies.Count);
+    }
+
+    [Test]
+    public void NamespaceNameTest()
+    {
+        Assert.AreEqual("AssemblyBrowserApplication.Model", _assemblyInfo.Namespacies[MODEL_NAMESPACE].Name);
+    }
+
+    [Test]
+    public void TypesAmountTest()
+    {
+        Assert.AreEqual(2, _assemblyInfo.Namespacies[MODEL_NAMESPACE].Types.Count);
+    }
+    
+    [Test]
+    public void PropertiesAmountTest()
+    {
+        Assert.AreEqual(1, _assemblyInfo.Namespacies[MODEL_NAMESPACE].Types[ASSEMBLY_MODEL_CLASS].Properties.Count);
+    }
+
+    [Test]
+    public void MethodInformationMethodCountTest()
+    {
+        Assert.AreEqual(6, _assemblyInfo.Namespacies[MODEL_NAMESPACE].Types[ASSEMBLY_MODEL_CLASS].Methods.Count);
+    }
+}
